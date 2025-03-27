3dcae266af6d88774a32ec01daa6e707ffdd31b3
Róisín Grannell <r.grannell2@gmail.com>
2025-03-27 19:53:27 +0000

    improved error handling

--- src/language.ts
+++ src/language.ts
@@ -1,45 +1,45 @@
+import _ from "lodash";
 
-import _ from 'lodash'
-
-import {
-  JordanPetersonException,
-  CarcinisationException
-} from './errors.js'
+import { CarcinisationException, JordanPetersonException } from "./errors.js";
 
 const jsToCrab = (content: string) => {
   return _([...content])
-    .map(char => {
+    .map((char: string) => {
       // -- probably won't handle delegate pairs well.
-      const charCode = char.charCodeAt(0)
-      return '🦀'.repeat(charCode)
+      const charCode = char.charCodeAt(0);
+      return "🦀".repeat(charCode);
     })
-    .join('\n')
-    .valueOf()
-}
+    .join("\n")
+    .valueOf();
+};
 
-const crabToJS = (content:string) => {
-  const charSet = new Set([...content])
+const crabToJS = (content: string) => {
+  const charSet = new Set([...content]);
 
-  if (charSet.has('🦞')) {
-    throw new JordanPetersonException('a lobster is not a crab')
-  } else if (!charSet.has('🦀') || charSet.size > 2) {
-    throw new CarcinisationException('there is only lines of crab.')
+  if (charSet.has("🦞")) {
+    throw new JordanPetersonException("a lobster is not a crab");
+  } else if (charSet.has("🦐")) {
+    throw new CarcinisationException("shrimp is bugs");
+  } else if (charSet.has("🍤")) {
+    throw new CarcinisationException("you monster!");
+  } else if (!charSet.has("🦀") || charSet.size > 2) {
+    throw new CarcinisationException("there is only lines of crab.");
   }
 
   return _([content])
-    .split('\n')
-    .map(crabs => {
-      return String.fromCharCode([...crabs].length)
+    .split("\n")
+    .map((crabs: string) => {
+      return String.fromCharCode([...crabs].length);
     })
-    .join('')
-    .valueOf()
-}
+    .join("")
+    .valueOf();
+};
 
 export default class CrabLang {
-  static encode (code:string) {
-    return jsToCrab(code)
+  static encode(code: string) {
+    return jsToCrab(code);
   }
-  static decode (code:string) {
-    return crabToJS(code)
+  static decode(code: string) {
+    return crabToJS(code);
   }
 }
